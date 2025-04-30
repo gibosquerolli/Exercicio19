e6038c8bfec01e0141e8259fdce2546801a9df83
Giovanna Bosquerolli <1bosquerolligiovanna@gmail.com>
2025-04-30 11:38:50 -0300

    Add files via upload

---
+++ Teste.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package exercicio19.teste;
+
+/**
+ *
+ * @author camil
+ */
+public class Teste {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
